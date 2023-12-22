3bf8d672a0972589f43cc743436a08ba2632ed40
Kaus1kC0des <kausikdevanathan@gmail.com>
2023-12-22 16:14:48 +0530

    Latest non-working commit

--- Data Science/Task 1/myflaskapp/app.py
+++ Data Science/Task 1/myflaskapp/app.py
@@ -1,41 +1,55 @@
-from flask import Flask, render_template, request, url_for, redirect,session
+import logging
+from flask import Flask, render_template, request, redirect, url_for
 from joblib import load
 
-
+# Set up logging
+logging.basicConfig(filename='logs/app.txt', level=logging.INFO)
 
 app = Flask(__name__)
 
+# Load model
+model = load("models/iris_model.joblib")
+global prediction
+
 @app.route("/")
 def home():
+    logging.info("Home page accessed")
     return render_template("home.html")
 
 
-@app.route("/predict",methods=["GET","POST"])
+@app.route("/predict", methods=["GET", "POST"])
 def predict():
     if request.method == "POST":
         try:
-            model = load("../../models/iris_model.joblib")
             sepal_length = float(request.form["sepal_length"])
             sepal_width = float(request.form["sepal_width"])
             petal_length = float(request.form["petal_length"])
             petal_width = float(request.form["petal_width"])
-            prediction = model.predict([[sepal_length,sepal_width,petal_length,petal_width]])
-            return redirect(url_for('output'))
+            features = [sepal_length, sepal_width, petal_length, petal_width]
+            prediction = model.predict([[features]])
+            if prediction[0] == 0:
+                prediction = "Setosa"
+            elif prediction[0] == 1:
+                prediction = "Versicolor"
+            else:
+                prediction = "Virginica"
+            return output(prediction)
+
         except Exception as e:
-            return render_template("error.html",error=e)
+            logging.error(f"Error occurred during prediction: {e}")
+            return render_template("error.html", error=e)
+
     elif request.method == "GET":
-        try:
-            return render_template("predict.html")
-        except Exception as e:
-            return render_template("error.html",error=e)
-
-@app.route("/output", methods=["GET"])
-def output():
-    prediction = session.get('prediction', None)
-    return render_template("output.html", prediction=prediction)
+        logging.info("Prediction page accessed")
+        return render_template("predict.html")
 
 
+@app.route("/output")
+def output(prediction):
+    logging.info(f"Output page accessed, prediction: {prediction}")
+    return render_template('output.html', prediction=prediction)
 
-if __name__=="__main__":
+
+
+if __name__ == "__main__":
     app.run()
-
